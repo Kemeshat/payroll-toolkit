c26e8d3533042f9d7a141c00be654ddf5fa3f946
Kemeshat <kemeshat@usf.edu>
2026-02-20 20:14:22 -0500

    Array created for employee objects.

---
+++ cc_5.js
@@ -0,0 +1,9 @@
+//Step 2: Employee Data
+const employees = [
+    { name: "Jane Doe", hourlyRate: 19, hoursWorked: 40 },
+    { name: "Bob Weller", hourlyRate: 30, hoursWorked: 35 },
+    { name: "Charlie Smith", hourlyRate: 15, hoursWorked: 45 },
+    { name: "Taylor Swift", hourlyRate: 20, hoursWorked: 30 }
+];
+console.log("Employee Data:", employees);
+
