b0f46d3289a458c3129c88182045a371eeb60eeb
Kemeshat <kemeshat@usf.edu>
2026-02-21 22:19:41 -0500

    Base pay and overtime pay calculated

--- cc_5.js
+++ cc_5.js
@@ -3,7 +3,9 @@ const employees = [
     { name: "Jane Doe", hourlyRate: 19, hoursWorked: 40 },
     { name: "Bob Weller", hourlyRate: 30, hoursWorked: 35 },
     { name: "Charlie Smith", hourlyRate: 15, hoursWorked: 45 },
-    { name: "Taylor Swift", hourlyRate: 20, hoursWorked: 30 }
+    { name: "Taylor Swift", hourlyRate: 20, hoursWorked: 50 }
 ];
 console.log("Employee Data:", employees);
+
+
 
